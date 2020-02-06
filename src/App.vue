b51ff81d72c26b0ed6568dcb3cecaea1fc7ed469
y84101978 <yifan.ren@huawei.com>
2020-02-06 12:05:47 +1100

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class = "container">
+    <div>
+      <TodoNew />
+      <TodoList />
+      <img alt="Vue logo" src="./assets/logo.png" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoNew from "@/components/TodoNew.vue";
+import TodoList from "@/components/TodoList.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    TodoNew,
+    TodoList
   }
-}
+};
 </script>
 
 <style lang="less">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+html {
+  font-family: "Open Sans", sans-serif;
+  background: linear-gradient(45deg, #5189c1 25%, #41b883);
+  height: 100%;
+  color: #333;
+}
+
+body {
+  display: flex;
+  height: 100%;
+  margin: 0;
+}
+
+.container {
+  width: 24rem;
+  margin: auto;
+  background-color: white;
+  border-radius: 1rem;
+  padding: 1rem;
+  box-shadow: 0 0 1rem rgba(0, 0, 0, 0.5);
+}
+
+h1 {
+  text-align: center;
+  margin-top: 0;
+}
+
+.vue-logo {
+  display: block;
+  width: 50px;
+  margin: 0 auto;
 }
 </style>
